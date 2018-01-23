fe086112c8ece1381278018a532d1166a9f018df
erica <jagg1@126.com>
2018-01-23 22:41:04 +0800

    修改

--- pratice36-进阶：把MVC的VC加到简历里/pratice36-02-模块化/js/init-swiper.js
+++ pratice36-进阶：把MVC的VC加到简历里/pratice36-02-模块化/js/init-swiper.js
@@ -1,26 +1,20 @@
 /**
  * Created by Administrator on 2018/1/23 0023.
  */
-!function(){
-    var mySwiper = new Swiper ('.swiper-container', {
+
+var mySwiper = new Swiper ('.swiper-container', {
 // Optional parameters
-        loop: true,
+    loop: true,
 
 // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
 // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-// And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    })
-}.call()
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+}
 
--- pratice36-进阶：把MVC的VC加到简历里/pratice36-02-模块化/js/sticky-topbar.js
+++ pratice36-进阶：把MVC的VC加到简历里/pratice36-02-模块化/js/sticky-topbar.js
@@ -1,6 +1,7 @@
 /**
  * Created by Administrator on 2018/1/23 0023.
  */
+
 window.addEventListener('scroll',function(x){
     if(window.scrollY > 0){
         topNavBar.classList.add('sticky')
