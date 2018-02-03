0058c5a27eaeb3f20c58c1d8fd4b575b73ca7ee4
erica <jagg1@126.com>
2018-02-03 23:35:21 +0800

    提交

--- 03senior/pratice39-高级：来，面向对象编程吧！/pratice39-02-分类controller/js/base/Controller.js
+++ 03senior/pratice39-高级：来，面向对象编程吧！/pratice39-02-分类controller/js/base/Controller.js
@@ -3,19 +3,25 @@
  */
 
 window.Controller = function(options){
-    var init = options.init
-    bindEvents = options.bindEvents
+    var init = options.init //A
 
-    return {
+    let object = {
         view:null,
         model:null,
         init:function(view,model){
             this.view = view
             this.model = model
             this.model.init()
-            options.bindEvents.call(this)
-            init.call(this,view,model)
+            init.call(this,view,model) //这个init是A处的init
+            this.bindEvents.call(this)
         }
     }
+
+    for(let key in options){
+        if(key !== 'init'){
+            object[key] = options[key]
+        }
+    }
+    return object
 }
 
