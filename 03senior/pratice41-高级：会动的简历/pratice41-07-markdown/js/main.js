7a9a74ea0673004c1972ab03b85467cfd182674c
erica <jagg1@126.com>
2018-02-10 15:57:31 +0800

    修改

--- 03senior/pratice41-高级：会动的简历/pratice41-07-markdown/js/main.js
+++ 03senior/pratice41-高级：会动的简历/pratice41-07-markdown/js/main.js
@@ -28,7 +28,7 @@ function writeCode(prefix,code,fn){
             window.clearInterval(id)
             fn && fn.call()
         }
-    },0)
+    },35)
 }
 
 function writeMarkdown(markdown,fn){
@@ -42,7 +42,7 @@ function writeMarkdown(markdown,fn){
             window.clearInterval(id)
             fn && fn.call()
         }
-    },70)
+    },35)
 }
 
 function convertMarkdownToHtml(fn){
@@ -55,7 +55,7 @@ function convertMarkdownToHtml(fn){
 }
 
 
-var result = `/*
+var result1 = `/*
 *面试官你好，我是xxx
 *我将以动画的形式来介绍我自己
 *只用文字介绍太单调了
@@ -97,29 +97,15 @@ html{
 
 /*不玩了，我来介绍一下我自己吧*/
 /*我需要一张白纸*/
-#code{
-    position:fixed;
-    left:0;
-    width:50%;
-    height:100%
-}
-#paper{
-    position:fixed;
-    right:0;
-    width:50%;
-    height:100%;
-    background:black;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    padding:16px;
+#code-wrapper{
+  width: 50%; left: 0; position: fixed;
+  height: 100%;
 }
 
 #paper > .content{
-    background:white;
-    height:100%;
-    width:100%;
+    display:block;
 }
+/* 于是我就可以在白纸上写字了，请看右边 */
 `
 
 var result2 = `
@@ -129,27 +115,73 @@ var result2 = `
 `
 
 var md = `
-#自我介绍
+# 自我介绍
 
-我叫XXX
-1999年1月出生
-XXX学校毕业
-自觉前端半年
+我叫 XXX
+1990 年 1 月出生
+XXX 学校毕业
+自学前端半年
 希望应聘前端开发岗位
 
-#技能介绍
-熟悉JavaScript CSS
+# 技能介绍
 
-#项目介绍
-1.XXX轮播
-2.XXX简历
-3.XXX画板
+熟悉 JavaScript CSS
 
-#联系方式
-QQ：XXXX
-Email:XXXX
-手机：XXXXX
+# 项目介绍
+
+1. XXX 轮播
+2. XXX 简历
+3. XXX 画板
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
 `
+
 let result3 = `
 /*
  * 这就是我的会动的简历
@@ -157,13 +189,12 @@ let result3 = `
  */
 `
 
-
-writeCode('',result,()=>{ //writeCode call the function
+writeCode('',result1,()=>{ //writeCode call the function
     createPaper(()=>{
         writeMarkdown(md,()=>{
-            writeCode(result,result2,()=>{
+            writeCode(result1,result2,()=>{
                 convertMarkdownToHtml(()=>{
-                    writeCode(result + result2, result3, ()=> {
+                    writeCode(result1 + result2, result3, ()=> {
                         console.log('完成')
                     })
                 })
@@ -171,3 +202,4 @@ writeCode('',result,()=>{ //writeCode call the function
         })
     })
 })
+
