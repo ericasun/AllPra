d692f3d7a1a4a38d5b9ad86647650f5969db0884
erica <jagg1@126.com>
2018-02-25 10:35:41 +0800

    新增

---
+++ 04graduation/01毕设——网易云音乐（直播11班版本）/07毕设：网易云音乐——首页——歌曲交互/index.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    setTimeout(function(){
+        $.get('./songs.json').then(function(response){
+            let items = response
+            items.forEach(i)=>{
+                let $li = $(`
+                <li>
+                    <a href="./song.html?id=${i.id}">
+                        <h3>${i.name}</h3>
+                        <p>演唱者-专辑</p>
+                        <svg class="play">
+                            <use xlink:href=""></use>  
+                        </svg>
+                    </a>
+                </li>
+            `)
+                $('#lastestMusic').append($li)
+            })
+        $('#lastestMusicLoading').remove()
+    },function(){
+            alert('失败')
+        })
+    },100)
+})
