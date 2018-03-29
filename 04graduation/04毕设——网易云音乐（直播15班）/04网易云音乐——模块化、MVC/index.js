85fe79290ed41a88d928264d36785416221aabdc
erica <jagg1@126.com>
2018-03-29 21:42:24 +0800

    mvc

--- 04graduation/04毕设——网易云音乐（直播15班）/04网易云音乐——模块化、MVC/index.js
+++ 04graduation/04毕设——网易云音乐（直播15班）/04网易云音乐——模块化、MVC/index.js
@@ -85,9 +85,33 @@ var server = http.createServer(function(request,response){
         response.write(string)
         response.end()
     }else if(path ==='/css/default.css'){
-        let string = fs.readFileSync('./css/default.css','utf-8')
+        let string = fs.readFileSync('./src/css/default.css','utf-8')
         response.statusCode = 200
         response.setHeader('Content-Type','text/css;charset=utf-8')
+        response.write(string)
+        response.end()
+    }else if(path ==='/js/new-song.js'){
+        let string = fs.readFileSync('./src/js/new-song.js','utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type','text/javascript;charset=utf-8')
+        response.write(string)
+        response.end()
+    }else if(path ==='/js/song-form.js'){
+        let string = fs.readFileSync('./src/js/song-form.js','utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type','text/javascript;charset=utf-8')
+        response.write(string)
+        response.end()
+    }else if(path ==='/js/song-list.js'){
+        let string = fs.readFileSync('./src/js/song-list.js','utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type','text/javascript;charset=utf-8')
+        response.write(string)
+        response.end()
+    }else if(path ==='/js/upload-song.js'){
+        let string = fs.readFileSync('./src/js/upload-song.js','utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type','text/javascript;charset=utf-8')
         response.write(string)
         response.end()
     }else{
