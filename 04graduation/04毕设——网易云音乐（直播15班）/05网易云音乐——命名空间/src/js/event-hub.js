02fb9fa2e1b360d254958eff4cfabf0cf5051535
erica <jagg1@126.com>
2018-04-01 22:52:58 +0800

    新增

---
+++ 04graduation/04毕设——网易云音乐（直播15班）/05网易云音乐——命名空间/src/js/event-hub.js
@@ -0,0 +1,17 @@
+window.eventHub = {
+    events:{
+        '羊城晚报':[],
+        '楚天都市报':[]
+    },
+    emit(){
+
+    },
+    on(eventName,fn){
+        for(let i = 0; i < this.events.length; i++){
+
+        }
+    },
+    off(){
+
+    },
+}
