c81652d78a61b401307da447f8fbc101a22fa558
erica <jagg1@126.com>
2018-04-07 15:42:29 +0800

    新增

---
+++ 04graduation/04毕设——网易云音乐（直播15班）/09网易云音乐——获取歌曲/src/js/song-list.js
@@ -0,0 +1,56 @@
+{
+    let view = {
+        el:'#songList-container',
+        template:`
+            <ul class="songList">
+            </ul>
+        `,
+        render(data){
+            let $el = $(this.el)
+            $el.html(this.template)
+            let {songs} = data
+            let liList = songs.map((song)=>$('<li></li>').text(song.name))
+            $el.find('ul').empty()
+            liList.map((domLi)=>{
+                $el.find('ul').append(domLi)
+            })
+        },
+        clearActive(){
+            $(this.el).find('.active').removeClass('active')
+        }
+    }
+    let model = {
+        data:{
+            songs:[]
+        },
+        find(){
+            var query = new AV.Query('Song');
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id:song.id,...song.attributes}
+                })
+                return songs
+            })
+        }
+    }
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+            window.eventHub.on('upload',()=>{
+                this.view.clearActive()
+            })
+            window.eventHub.on('create',(songData)=>{
+                this.model.data.songs.push(songData)
+                this.view.render(this.model.data)
+            })
+            this.model.find().then(()=>{
+                console.log("----------")
+                console.log(this.model.data)
+                this.view.render(this.model.data)
+            })
+        }
+    }
+    controller.init(view,model)
+}
