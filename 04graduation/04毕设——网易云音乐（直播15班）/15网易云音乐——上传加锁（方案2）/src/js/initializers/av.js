ae97dcd0320854bfad90e6af9d04b45df388aa83
erica <jagg1@126.com>
2018-04-08 20:57:47 +0800

    新增

---
+++ 04graduation/04毕设——网易云音乐（直播15班）/15网易云音乐——上传加锁（方案2）/src/js/initializers/av.js
@@ -0,0 +1,10 @@
+{
+    let APP_ID = 'mwY7FhH8cQ7GUpuEMFeKlSPu-gzGzoHsz';
+    let APP_KEY = 'uHr7SsrO2iJ1YyOEGiszdxIr';
+
+    AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+    });
+
+}
